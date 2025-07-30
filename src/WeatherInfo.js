243bce058dc1b8ff2ffc3c53f285b7e227b41364
KayRams <ramolotoingrid@gmail.com>
2025-07-30 04:27:19 +0200

    Updated the unit toggle function

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
+
 export default function WeatherInfo(props) {
+  const [unit, setUnit] = useState("Celsius");
+
+  function convertToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  }
+
+  function handleCelsiusClick(event) {
+    event.preventDefault();
+    setUnit("C");
+  }
+
+  function handleFahrenheitClick(event) {
+    event.preventDefault();
+    setUnit("F");
+  }
+
+  let temperature = props.data.temperature;
+  if (unit === "F") {
+    temperature = convertToFahrenheit(temperature);
+  }
+
   return (
     <div className="WeatherInfo">
       <div className="container">
         <div className="row mt-5 mb-3">
-          <div className="col-6">
+          <div className="col-3 pe-0 text-end">
             <strong>
-              <h3>{Math.round(props.data.temperature)}°C</h3>
+              <h3>{Math.round(temperature)}</h3>
             </strong>
+          </div>
+          <div className="col-3 ps-0" id="unit">
+            <a href="/" onClick={handleCelsiusClick}>
+              °C
+            </a>{" "}
+            |{" "}
+            <a href="/" onClick={handleFahrenheitClick}>
+              °F
+            </a>
           </div>
           <div className="col-6">
             <img src={props.data.iconUrl} alt={props.data.description} />
