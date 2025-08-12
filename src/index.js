4a13f8bdb49df321ab13a4871115b4e3e1ee97d6
KayRams <ramolotoingrid@gmail.com>
2025-08-12 04:27:13 +0200

    Updated the footer

--- src/index.js
+++ src/index.js
@@ -10,14 +10,24 @@ root.render(
   <React.StrictMode>
     <App />
     <footer>
-      Open-sourced on{" "}
+      Open-sourced by{" "}
       <strong>
         <a
           href="https://github.com/KayRams/react-weather-app"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Github
+          Ingrid Ramoloto
+        </a>
+      </strong>{" "}
+      and hosted on{" "}
+      <strong>
+        <a
+          href="https://react-weather-app19.netlify.app/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Netlify
         </a>
       </strong>
     </footer>
